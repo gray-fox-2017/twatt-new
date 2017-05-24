e34c8d3d6520f854023f84ab836d37d17e4a30e6
stedyyulius <stedyyulius@gmail.com>
2017-05-24 12:54:22 +0700

    done

--- Controllers/controller-twatt.js
+++ Controllers/controller-twatt.js
@@ -3,11 +3,12 @@ require('dotenv').config()
 
 function twattPost (req,res,next){
   twatt.Auth(oauth=>{
+    let post = `Title ${req.body.title} by ${req.body.username}`
     oauth.post(
-                `https://api.twitter.com/1.1/statuses/update.json?status=Storyoverflow Title ${req.body.title} by ${req.body.username}`,
+                `https://api.twitter.com/1.1/statuses/update.json?status=www.storyoverflow-grayfox.herokuapp.com\nTitle ${req.body.title} by ${req.body.username}`,
                 process.env.Access_token,
                 process.env.Access_token_secret,
-                req.body.title,
+                post,
                 'text',
                 function(e, data) {
                   if (e) console.error(data);
